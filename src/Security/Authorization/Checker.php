116b80323e2d367df3809c3ef696302ae205ee5f
Artem Sykchin <artem.sykchin@dataproboston.com>
2017-12-25 20:31:16 +0200

    implement isGranted

--- src/Security/Authorization/Checker.php
+++ src/Security/Authorization/Checker.php
@@ -4,20 +4,44 @@ namespace Security\Authorization;
 
 
 use Config\ConfigInterface;
+use Exception\ForbiddenHttpException;
 use Security\Authentication\User;
 use Router\Route;
 
 class Checker
 {
+    /** @var ConfigInterface  */
     private $config;
 
+    /**
+     * Checker constructor.
+     * @param ConfigInterface $config
+     */
     public function __construct(ConfigInterface $config)
     {
         $this->config = $config;
     }
 
+    /**
+     * @param Route $route
+     * @param User $user
+     * @return bool
+     * @throws ForbiddenHttpException
+     */
     public function isGranted(Route $route, User $user)
     {
-        return true;
+        foreach ($this->config->toArray() as $accessItem) {
+            $pattern = '@^' . $accessItem['path'] . '/?$@i';
+
+            if (!preg_match($pattern, $route->getUrl())) {
+                continue;
+            }
+
+            if (count(array_intersect($user->getRoles(), $accessItem['roles'])) > 0) {
+                return true;
+            }
+        }
+        
+        throw new ForbiddenHttpException('Forbidden');
     }
 }
--- src/Security/Handler.php
+++ src/Security/Handler.php
@@ -9,18 +9,32 @@ use Security\Authentication\Provider\ProviderInterface;
 
 class Handler
 {
+    /** @var Checker  */
     private $authorizationChecker;
+
+    /** @var ProviderInterface  */
     private $authenticationProvider;
 
+    /**
+     * Handler constructor.
+     * @param ProviderInterface $authenticationProvider
+     * @param Checker $authorizationChecker
+     */
     public function __construct(ProviderInterface $authenticationProvider, Checker $authorizationChecker)
     {
         $this->authorizationChecker = $authorizationChecker;
         $this->authenticationProvider = $authenticationProvider;
     }
 
+    /**
+     * @param Route $route
+     * @return bool
+     * @throws \Exception\ForbiddenHttpException
+     */
     public function handle(Route $route)
     {
         $user = $this->authenticationProvider->authenticate();
+
         return $this->authorizationChecker->isGranted($route, $user);
     }
 
